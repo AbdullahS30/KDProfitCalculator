3d702cb94a7c1d87fcc75f2ad1dba76fe8bc042c
AbdullahS30 <abdullah.sarfraz@din.global>
2025-02-09 19:06:10 +0500

    fix: home grid

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </p>
         </header>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
           <Card>
             <CardHeader>
               <CardTitle className="flex items-center gap-2">
